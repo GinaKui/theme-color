6a584cba8d22936d65418dd7a0acbab9bfd8f77a
ginakui <developerkui@outlook.com>
2020-01-21 20:43:33 -0500

    add react router

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { Route, Switch } from 'react-router-dom';
 import Palette from './Palette';
 import seedColors from './seedColors';
 import { generatePalette } from "./colorHelpers";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <Palette palette={generatePalette(seedColors[4])}/>
-    </div>
-  );
+export default class App extends {
+  render() {
+    return (
+      <Switch>
+        <Route exact path="/" />
+        <Route exact path="/palette/:id" render={() => {
+          <div className="App">
+            <Palette palette={generatePalette(seedColors[4])}/>
+          </div>
+        }}/>
+      </Switch>
+      
+    );
+  }
 }
 
-export default App;
+
