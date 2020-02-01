100505bf3cd7757951831fba2f1592b6deaa755c
ginakui <developerkui@outlook.com>
2020-02-01 12:10:26 -0500

    finish styling MiniPalette

--- src/PaletteList.js
+++ src/PaletteList.js
@@ -5,7 +5,7 @@ import MiniPalette from './MiniPalette';
 const styles = {
   root: {
     backgroundColor: 'darkblue',
-    height: '100%',
+    height: '100vh',
     display: 'flex',
     alignItems: 'flex-start',
     justifyContent: 'center'
