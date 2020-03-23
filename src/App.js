17017f3a60e7c943cb3c476029cca29d17643c05
ginakui <developerkui@outlook.com>
2020-03-23 11:10:21 -0400

    add NewPlatteForm

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Route, Switch } from 'react-router-dom';
 import PaletteList from './PaletteList';
 import Palette from './Palette';
 import SingleColorPalette from './SingleColorPalette';
+import NewPaletteForm from './NewPaletteForm'
 import seedColors from './seedColors';
 import { generatePalette } from "./colorHelpers";
 import './App.css';
@@ -18,14 +19,24 @@ export default class App extends Component {
       <Switch>
         <Route 
           exact 
+          path="/palette/new" 
+          render={() => (
+            <NewPaletteForm />
+          )}
+        />
+        <Route 
+          exact 
           path="/" 
-          render={routeProps => <PaletteList palettes={seedColors} {...routeProps} />} 
+          render={routeProps => (
+            <PaletteList palettes={seedColors} {...routeProps} />
+          )}
         />
         <Route 
           exact 
           path="/palette/:id" 
           render={routeProps => ( 
-            <Palette palette={generatePalette(this.findPalette(routeProps.match.params.id))} />        
+            <Palette
+             palette={generatePalette(this.findPalette(routeProps.match.params.id))} />        
           )}
         />
         <Route
