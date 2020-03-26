115b8c152c943a24b32eb78fec48cf534b0a35f6
ginakui <developerkui@outlook.com>
2020-03-26 15:24:48 -0400

    add responsive helper and style ColorBox

---
+++ src/styles/sizes.js
@@ -0,0 +1,13 @@
+export default {
+  up() {
+  },
+  down(size) {
+    const breakPoints = {
+      xs: '575.98px',
+      sm: '767.98px',
+      md: '991.98px',
+      lg: '1199.98px'
+    };
+    return `@media (max-width: ${breakPoints[size]})`;
+  }
+}
